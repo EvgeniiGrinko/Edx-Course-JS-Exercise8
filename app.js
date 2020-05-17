96d448f60dac98e7763318ead9e225fe315ada90
Evgenii <jenek0907@mail.ru>
2020-05-17 12:25:14 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var numberInput = document.getElementById("number");
+var validationMessage = document.getElementById("validationMessage");
+function verify() 
+{
+    if(numberInput.value < 40 || numberInput.value > 90){
+        validationMessage.style.display = "block";
+        return false;    }
+
+else {
+    validationMessage.style.display = "none";
+    return true;
+}
+}
+
+numberInput.onchange = function(){
+    if(isNaN(numberInput.value)){
+      nanMessage.style.display = "block";
+    }
+    else{
+      nanMessage.style.display = "none";
+    }
+  }
